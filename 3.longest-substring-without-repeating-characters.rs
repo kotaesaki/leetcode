d565d7d689b1203f7b282066f4930e4564b47093
kotaesaki <jmkamy552@gmail.com>
2022-11-07 23:04:13 +0900

    3 done rust

--- 3.longest-substring-without-repeating-characters.rs
+++ 3.longest-substring-without-repeating-characters.rs
@@ -5,25 +5,43 @@
  */
 
 // @lc code=start
+use std::cmp::max;
 impl Solution {
     pub fn length_of_longest_substring(s: String) -> i32 {
-        const RADIX: u32 = 10;
-        let mut vec: Vec<char> = vec![];
-        let mut result: u8;
-        for i in s.as_str().chars() {
-            println!("{}", i.to_digit(RADIX).unwrap());
-            // if !vec.iter().any(|e| e == i.to_digit(RADIX).unwrap()) {
-            //     vec.push(i);
-            // } else {
-            //     if result < vec.len() {
-            //         result = vec.len() as u8;
-            //     }
-            //     vec = vec![];
-            //     break;
-            // }
-        }
-        // vec.len()
-        3
+        let mut current: Vec<char> = vec![];
+        let mut max_length = 0;
+
+        s.chars().for_each(|c| {
+            if let Some(i) = current.iter().position(|&x| x == c) {
+                current.drain(0..i + 1);
+            }
+            current.push(c);
+            max_length = max(current.len(), max_length);
+        });
+        max_length as i32
+        // let mut vec: Vec<char> = vec![];
+
+        // let mut result: usize = 0;
+        // for i in s.as_str().chars() {
+        //     if !vec.iter().any(|e| *e == i) {
+        //         vec.push(i);
+        //     } else {
+        //         if result < vec.len() {
+        //             result = vec.len();
+        //         };
+        //         for v in &vec {
+        //             vec.remove(0);
+        //             if *v == i {
+        //                 break;
+        //             }
+        //         }
+        //         vec.push(i);
+        //     }
+        // }
+        // if result < vec.len() {
+        //     result = vec.len();
+        // }
+        // result as i32
     }
 }
 // @lc code=end
