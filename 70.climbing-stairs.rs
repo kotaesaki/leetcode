4fa5c0ce3c517b0710d3bbbe15c06eec723350b0
kotaesaki <jmkamy552@gmail.com>
2022-11-08 23:08:30 +0900

    70 rust.

---
+++ 70.climbing-stairs.rs
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=70 lang=rust
+ *
+ * [70] Climbing Stairs
+ */
+
+// @lc code=start
+impl Solution {
+    pub fn climb_stairs(n: i32) -> i32 {
+        let mut fb Vec<i32> = vec![1, 2];
+
+    }
+}
+// @lc code=end
