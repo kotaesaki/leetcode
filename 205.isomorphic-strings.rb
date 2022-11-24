0f69a7c1759075e7cc8bcd30a380dab031ec3322
kotaesaki <jmkamy552@gmail.com>
2022-11-24 21:00:00 +0900

    wip 205

---
+++ 205.isomorphic-strings.rb
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode id=205 lang=ruby
+#
+# [205] Isomorphic Strings
+#
+
+# @lc code=start
+# @param {String} s
+# @param {String} t
+# @return {Boolean}
+def is_isomorphic(s, t)
+    
+end
+# @lc code=end
+
