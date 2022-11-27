08400d94e3a072402661b596b735c480b89fa955
kotaesaki <jmkamy552@gmail.com>
2022-11-27 16:30:55 +0900

    done 205

--- 205.isomorphic-strings.rb
+++ 205.isomorphic-strings.rb
@@ -9,7 +9,17 @@
 # @param {String} t
 # @return {Boolean}
 def is_isomorphic(s, t)
-    
+  memo = {}
+  s_array = s.chars
+  t_array = t.chars
+  s_array.each_with_index do |char, index|
+    return false if memo[char] && (memo[char] != t_array[index])
+    return false if !memo[char] && memo.key(t_array[index])
+
+    memo[char] = t_array[index]
+  end
+  p memo
+  true
 end
 # @lc code=end
 
