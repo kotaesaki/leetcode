df6889276e2da2d994f986a1ddc14c549e1301b1
kotaesaki <jmkamy552@gmail.com>
2022-11-30 18:37:21 +0900

    qip 219

---
+++ 219.contains-duplicate-ii.rb
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode id=219 lang=ruby
+#
+# [219] Contains Duplicate II
+#
+
+# @lc code=start
+# @param {Integer[]} nums
+# @param {Integer} k
+# @return {Boolean}
+def contains_nearby_duplicate(nums, k)
+  
+end
+# @lc code=end
+
