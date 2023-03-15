940321aa1ab3a4a0db820acff81fd12af5da3856
kotaesaki <jmkamy552@gmail.com>
2023-03-15 23:10:25 +0900

    ２個目のやり方

--- 242.valid-anagram.go
+++ 242.valid-anagram.go
@@ -13,14 +13,9 @@ func isAnagram(s string, t string) bool {
 	if len(s) != len(t) {
 		return false
 	}
-	chars1 := []rune(s)
-	s1 := []string{}
-	for i := 0; i < len(chars1); i++ {
-		s1 = append(s1, string(chars1[i]))
-	}
-	for _, v := range s1 {
-		if strings.Contains(t, v) {
-			t = strings.Replace(t, v, "", 1)
+	for _, v := range s {
+		if strings.Contains(t, string(v)) {
+			t = strings.Replace(t, string(v), "", 1)
 		} else {
 			return false
 		}
