6cb7804bb41e397721654993731716e60b427414
kotaesaki <jmkamy552@gmail.com>
2023-03-15 23:37:22 +0900

    天才

--- 242.valid-anagram.go
+++ 242.valid-anagram.go
@@ -1,6 +1,6 @@
 package main
 
-import "strings"
+import "fmt"
 
 /*
  * @lc app=leetcode id=242 lang=golang
@@ -10,16 +10,36 @@ import "strings"
 
 // @lc code=start
 func isAnagram(s string, t string) bool {
-	if len(s) != len(t) {
-		return false
-	}
+	// if len(s) != len(t) {
+	// 	return false
+	// }
+	// for _, v := range s {
+	// 	if strings.Contains(t, string(v)) {
+	// 		t = strings.Replace(t, string(v), "", 1)
+	// 	} else {
+	// 		return false
+	// 	}
+	// }
+	// return true
+
+	chars := make([]int, 26)
 	for _, v := range s {
-		if strings.Contains(t, string(v)) {
-			t = strings.Replace(t, string(v), "", 1)
-		} else {
+		i := int(v - 'a')
+		chars[i]++
+		fmt.Println(chars)
+	}
+
+	for _, v := range t {
+		i := int(v - 'a')
+		chars[i]--
+	}
+
+	for _, v := range chars {
+		if v != 0 {
 			return false
 		}
 	}
+
 	return true
 }
 
