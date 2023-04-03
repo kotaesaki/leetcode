490ecb14b26e8b758f24de9563d4c59549652cca
kotaesaki <jmkamy552@gmail.com>
2023-04-03 22:07:06 +0900

    344 done

---
+++ 344.reverse-string.go
@@ -0,0 +1,16 @@
+package main
+
+/*
+ * @lc app=leetcode id=344 lang=golang
+ *
+ * [344] Reverse String
+ */
+
+// @lc code=start
+func reverseString(s []byte) {
+	for i, j := 0, len(s)-1; i < len(s)/2; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
+}
+
+// @lc code=end
