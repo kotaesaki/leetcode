bd1dea70925bd382dabdd03a3a94908a88d73291
kotaesaki <jmkamy552@gmail.com>
2023-04-04 20:27:59 +0900

    367 done

---
+++ 367.valid-perfect-square.go
@@ -0,0 +1,19 @@
+package main
+
+/*
+ * @lc app=leetcode id=367 lang=golang
+ *
+ * [367] Valid Perfect Square
+ */
+
+// @lc code=start
+func isPerfectSquare(num int) bool {
+	for i := 1; i*i <= num; i++ {
+		if i*i == num {
+			return true
+		}
+	}
+	return false
+}
+
+// @lc code=end
