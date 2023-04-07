9b58b65636129a1d8e2dbe0e47214939d5e499d7
kotaesaki <jmkamy552@gmail.com>
2023-04-07 14:15:22 +0900

    wip

---
+++ 389.find-the-difference.go
@@ -0,0 +1,20 @@
+package main
+
+/*
+ * @lc app=leetcode id=389 lang=golang
+ *
+ * [389] Find the Difference
+ */
+
+// @lc code=start
+func findTheDifference(s string, t string) byte {
+	// char_list := make([]int, 26)
+	for i, _ := range t {
+		if s[i:i+1] != t[i:i+1] {
+			return i
+		}
+	}
+	return -1
+}
+
+// @lc code=end
