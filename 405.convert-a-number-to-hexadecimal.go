1f90bd2eb24537035d0866c2e364904388d8ca8b
kotaesaki <jmkamy552@gmail.com>
2023-04-10 22:04:39 +0900

    wip

---
+++ 405.convert-a-number-to-hexadecimal.go
@@ -0,0 +1,14 @@
+package main
+
+/*
+ * @lc app=leetcode id=405 lang=golang
+ *
+ * [405] Convert a Number to Hexadecimal
+ */
+
+// @lc code=start
+func toHex(num int) string {
+
+}
+
+// @lc code=end
