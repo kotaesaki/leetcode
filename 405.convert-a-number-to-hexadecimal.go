3b7ab1370f19fff6bc3d433582f34a9f530c93ad
kotaesaki <jmkamy552@gmail.com>
2023-04-11 18:35:25 +0900

    done

--- 405.convert-a-number-to-hexadecimal.go
+++ 405.convert-a-number-to-hexadecimal.go
@@ -1,5 +1,7 @@
 package main
 
+import "strings"
+
 /*
  * @lc app=leetcode id=405 lang=golang
  *
@@ -8,7 +10,13 @@ package main
 
 // @lc code=start
 func toHex(num int) string {
-
+	list := []string{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"}
+	var num16 []string
+	for num != 0 {
+		num16 = append([]string{list[num%16]}, num16...)
+		num /= 16
+	}
+	return strings.Join(num16, "")
 }
 
 // @lc code=end
