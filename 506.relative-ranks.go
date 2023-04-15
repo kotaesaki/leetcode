50a26f2ce84954993fb502d98f912e00ab4ec38a
kotaesaki <jmkamy552@gmail.com>
2023-04-15 16:18:51 +0900

    506 done

--- 506.relative-ranks.go
+++ 506.relative-ranks.go
@@ -1,3 +1,10 @@
+package main
+
+import (
+	"sort"
+	"strconv"
+)
+
 /*
  * @lc app=leetcode id=506 lang=golang
  *
@@ -6,7 +13,37 @@
 
 // @lc code=start
 func findRelativeRanks(score []int) []string {
-    
-}
-// @lc code=end
+	result := make([]string, len(score))
+	new_score := make([]int, len(score))
+	copy(new_score, score)
+	sort.Sort(sort.Reverse(sort.IntSlice(score)))
 
+	for i, v := range score {
+		index := findScoreIndex(v, new_score)
+		if i == 0 {
+			result[index] = "Gold Medal"
+			continue
+		}
+		if i == 1 {
+			result[index] = "Silver Medal"
+			continue
+		}
+		if i == 2 {
+			result[index] = "Bronze Medal"
+			continue
+		}
+		result[index] = strconv.Itoa(i + 1)
+	}
+	return result
+}
+
+func findScoreIndex(value int, new_score []int) int {
+	for i, v := range new_score {
+		if value == v {
+			return i
+		}
+	}
+	panic("panic")
+}
+
+// @lc code=end
