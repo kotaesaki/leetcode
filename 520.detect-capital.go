48037e127c9b973fc1089abc2e81cf6558a4b18f
kotaesaki <jmkamy552@gmail.com>
2023-04-18 20:19:01 +0900

    520 done

--- 520.detect-capital.go
+++ 520.detect-capital.go
@@ -1,3 +1,5 @@
+package main
+
 /*
  * @lc app=leetcode id=520 lang=golang
  *
@@ -6,7 +8,33 @@
 
 // @lc code=start
 func detectCapitalUse(word string) bool {
-    
+	if len(word) == 1 {
+		return true
+	}
+	output := make([]int, len(word))
+	for i, r := range word {
+		if r < 97 {
+			output[i] = 0
+		} else {
+			output[i] = 1
+		}
+	}
+	if output[0] == 1 {
+		return !find(output, 0)
+	}
+	if output[1] == 0 {
+		return !find(output, 1)
+	}
+	return !find(output[1:], 0)
 }
-// @lc code=end
 
+func find(output []int, value int) bool {
+	for _, v := range output {
+		if value == v {
+			return true
+		}
+	}
+	return false
+}
+
+// @lc code=end
