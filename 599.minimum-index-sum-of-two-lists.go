fe3e5773e400f2360a44469aece1deb2da901235
kotaesaki <jmkamy552@gmail.com>
2023-04-24 08:55:50 +0900

    wip

---
+++ 599.minimum-index-sum-of-two-lists.go
@@ -0,0 +1,12 @@
+/*
+ * @lc app=leetcode id=599 lang=golang
+ *
+ * [599] Minimum Index Sum of Two Lists
+ */
+
+// @lc code=start
+func findRestaurant(list1 []string, list2 []string) []string {
+    
+}
+// @lc code=end
+
