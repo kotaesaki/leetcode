62d13d9cad2e5048779e0653f030e8669df4eb56
kotaesaki <jmkamy552@gmail.com>
2023-04-24 20:26:54 +0900

    done

--- 599.minimum-index-sum-of-two-lists.go
+++ 599.minimum-index-sum-of-two-lists.go
@@ -1,3 +1,5 @@
+package main
+
 /*
  * @lc app=leetcode id=599 lang=golang
  *
@@ -6,7 +8,30 @@
 
 // @lc code=start
 func findRestaurant(list1 []string, list2 []string) []string {
-    
+	output := []string{}
+	resultIndex := 2001
+	for i, v := range list2 {
+		index := findIndex(list1, v)
+		if index == nil || resultIndex < i+(*index) {
+			continue
+		}
+		sumIndex := i + (*index)
+		if resultIndex > sumIndex {
+			resultIndex = sumIndex
+			output = nil
+		}
+		output = append(output, v)
+	}
+	return output
 }
-// @lc code=end
 
+func findIndex(list []string, value string) *int {
+	for i, v := range list {
+		if value == v {
+			return &i
+		}
+	}
+	return nil
+}
+
+// @lc code=end
