df7c86c56c43b54c7f981113a56daaf0b3eb713f
kotaesaki <jmkamy552@gmail.com>
2023-04-25 20:46:35 +0900

    605 done

--- 605.can-place-flowers.go
+++ 605.can-place-flowers.go
@@ -8,7 +8,33 @@ package main
 
 // @lc code=start
 func canPlaceFlowers(flowerbed []int, n int) bool {
+	if n == 0 {
+		return true
+	}
+	count := 0
+	for i, v := range flowerbed {
+		if v == 1 {
+			continue
+		}
+		left, right := i-1, i+1
 
+		if left >= 0 {
+			if flowerbed[left] == 1 {
+				continue
+			}
+		}
+		if right != len(flowerbed) {
+			if flowerbed[right] == 1 {
+				continue
+			}
+		}
+		flowerbed[i] = 1
+		count++
+		if n == count {
+			return true
+		}
+	}
+	return false
 }
 
 // @lc code=end
