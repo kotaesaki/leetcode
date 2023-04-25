d83eb678427da92cefa4a7e54ef6a7c305ba6865
kotaesaki <jmkamy552@gmail.com>
2023-04-25 09:08:47 +0900

    wip

---
+++ 605.can-place-flowers.go
@@ -0,0 +1,14 @@
+package main
+
+/*
+ * @lc app=leetcode id=605 lang=golang
+ *
+ * [605] Can Place Flowers
+ */
+
+// @lc code=start
+func canPlaceFlowers(flowerbed []int, n int) bool {
+
+}
+
+// @lc code=end
