b9040d716dd72f19b7a06fe7a0812eaabb731e96
kotaesaki <jmkamy552@gmail.com>
2023-05-09 08:29:44 +0900

    done 657 for golang.

--- 657.robot-return-to-origin.go
+++ 657.robot-return-to-origin.go
@@ -8,7 +8,20 @@ package main
 
 // @lc code=start
 func judgeCircle(moves string) bool {
-	return true
+	x, y := 0, 0
+
+	for _, r := range moves {
+		if 'U' == r {
+			y += 1
+		} else if 'D' == r {
+			y -= 1
+		} else if 'R' == r {
+			x += 1
+		} else {
+			x -= 1
+		}
+	}
+	return x == 0 && y == 0
 }
 
 // @lc code=end
