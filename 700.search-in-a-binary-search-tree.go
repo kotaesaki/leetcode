3b324109444251b4cc4ce2bf53f7e039512f46d5
kotaesaki <jmkamy552@gmail.com>
2023-05-12 08:55:34 +0900

    done 700 in go

---
+++ 700.search-in-a-binary-search-tree.go
@@ -0,0 +1,31 @@
+package main
+
+/*
+ * @lc app=leetcode id=700 lang=golang
+ *
+ * [700] Search in a Binary Search Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func searchBST(root *TreeNode, val int) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	if root.Val == val {
+		return root
+	}
+	if root.Val < val {
+		return searchBST(root.Right, val)
+	}
+	return searchBST(root.Left, val)
+}
+
+// @lc code=end
