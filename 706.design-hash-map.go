5046851ec169d14347d6017b3214aa599b8225bb
kotaesaki <jmkamy552@gmail.com>
2023-05-15 20:17:58 +0900

    done

---
+++ 706.design-hash-map.go
@@ -0,0 +1,38 @@
+package main
+
+/*
+ * @lc app=leetcode id=706 lang=golang
+ *
+ * [706] Design HashMap
+ */
+
+// @lc code=start
+type MyHashMap struct {
+	key   int
+	value int
+}
+
+func Constructor() MyHashMap {
+	return MyHashMap{}
+}
+
+func (this *MyHashMap) Put(key int, value int) {
+
+}
+
+func (this *MyHashMap) Get(key int) int {
+
+}
+
+func (this *MyHashMap) Remove(key int) {
+
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Put(key,value);
+ * param_2 := obj.Get(key);
+ * obj.Remove(key);
+ */
+// @lc code=end
---
+++ 709.to-lower-case.rb
@@ -0,0 +1,14 @@
+#
+# @lc app=leetcode id=709 lang=ruby
+#
+# [709] To Lower Case
+#
+
+# @lc code=start
+# @param {String} s
+# @return {String}
+def to_lower_case(s)
+  s.downcase || s
+end
+# @lc code=end
+
