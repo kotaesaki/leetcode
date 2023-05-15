a7914f023f3e4d3b61b7afccbfcfab6dada52c58
kotaesaki <jmkamy552@gmail.com>
2023-05-15 20:19:57 +0900

    done

---
+++ 709.to-lower-case.go
@@ -0,0 +1,16 @@
+package main
+
+import "strings"
+
+/*
+ * @lc app=leetcode id=709 lang=golang
+ *
+ * [709] To Lower Case
+ */
+
+// @lc code=start
+func toLowerCase(s string) string {
+	return strings.ToLower(s)
+}
+
+// @lc code=end
