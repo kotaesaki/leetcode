b00638916d90664ebeb37eced34c132d0f69d49f
kotaesaki <jmkamy552@gmail.com>
2023-05-17 19:28:13 +0900

    done in go

---
+++ 746.min-cost-climbing-stairs.rb
@@ -0,0 +1,29 @@
+#
+# @lc app=leetcode id=746 lang=ruby
+#
+# [746] Min Cost Climbing Stairs
+#
+
+# @lc code=start
+# @param {Integer[]} cost
+# @return {Integer}
+def min_cost_climbing_stairs(cost)
+  calculate(cost)
+end
+
+def calculate(cost)
+  dp = []
+  cost.each_with_index do |c, index|
+    next dp << c if [0, 1].include?(index)
+
+    if dp[index-1] > dp[index-2]
+      dp << (c + dp[index-2])
+    else
+      dp << (c + dp[index-1])
+    end
+  end
+
+  dp[-1] < dp[-2] ? dp[-1] : dp[-2]
+end
+# @lc code=end
+
