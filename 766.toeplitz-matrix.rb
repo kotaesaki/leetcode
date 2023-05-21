7a593ad9def793e2487dcc98bedb895be1baded9
kotaesaki <jmkamy552@gmail.com>
2023-05-21 21:05:00 +0900

    wip

---
+++ 766.toeplitz-matrix.rb
@@ -0,0 +1,14 @@
+#
+# @lc app=leetcode id=766 lang=ruby
+#
+# [766] Toeplitz Matrix
+#
+
+# @lc code=start
+# @param {Integer[][]} matrix
+# @return {Boolean}
+def is_toeplitz_matrix(matrix)
+    
+end
+# @lc code=end
+
