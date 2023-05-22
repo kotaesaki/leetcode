aa2691b0b323d6fce6e33bee05e82e4f3ea906d9
kotaesaki <jmkamy552@gmail.com>
2023-05-22 19:50:16 +0900

    done

--- 766.toeplitz-matrix.rb
+++ 766.toeplitz-matrix.rb
@@ -8,7 +8,15 @@
 # @param {Integer[][]} matrix
 # @return {Boolean}
 def is_toeplitz_matrix(matrix)
-    
+  matrix.length.times do |i|
+    next if i == 0
+
+    matrix[i].length.times do |j|
+      next if j == 0
+      return false if matrix[i][j] != matrix[i-1][j-1]
+    end
+  end
+  return true
 end
 # @lc code=end
 
