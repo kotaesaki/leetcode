c88b95ae83220fa312c43a39c682a14570861cba
kotaesaki <jmkamy552@gmail.com>
2023-05-25 21:36:24 +0900

    done

--- 836.rectangle-overlap.rb
+++ 836.rectangle-overlap.rb
@@ -9,14 +9,15 @@
 # @param {Integer[]} rec2
 # @return {Boolean}
 def is_rectangle_overlap(rec1, rec2)
-  # puts "[rec2[0]..rec2[2]]: #{rec2[0]..rec2[2]}"
-
-  # (rec2[0]..rec2[2]).each do |x|
-  #   puts "x: #{x}"
-  #   puts "rec1[0]+1..rec1[2]-1: #{rec1[0]+1..rec1[2]-1}"
-  #   puts (rec1[0]+1..rec1[2]-1).include?(x)
-  # end
-  (rec2[0]..rec2[2]).find { |x| (rec1[0]+1..rec1[2]-1).include?(x) } == 1 && (rec2[1]..rec2[3]).find { |y| (rec1[1]+1..rec1[3]-1).include?(y) } == 1
+  # Check if the rectangles do not overlap
+  if rec1[2] <= rec2[0] || # left edge of rec1 is to the left of right edge of rec2
+      rec1[0] >= rec2[2] || # right edge of rec1 is to the right of left edge of rec2
+      rec1[3] <= rec2[1] || # bottom edge of rec1 is below top edge of rec2
+      rec1[1] >= rec2[3]    # top edge of rec1 is above bottom edge of rec2
+      return false
+  end
+  return true
 end
 # @lc code=end
 
+# "[7,8,13,15]\n[10,8,12,20]"
