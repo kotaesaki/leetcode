46ce01a88ed2b63704bb3440a25dc43cc452788b
kotaesaki <jmkamy552@gmail.com>
2023-05-31 09:03:55 +0900

    done in ruby

---
+++ 860.lemonade-change.rb
@@ -0,0 +1,38 @@
+#
+# @lc app=leetcode id=860 lang=ruby
+#
+# [860] Lemonade Change
+#
+
+# @lc code=start
+# @param {Integer[]} bills
+# @return {Boolean}
+def lemonade_change(bills)
+  h = {5 => 0, 10 => 0, 20 => 0}
+  bills.each do |bill|
+    case bill
+    when 5
+      h[5] += 1
+    when 10
+      return false if h[5] <= 0
+      h[5] -= 1
+      h[10] += 1
+    when 20
+      if h[10] >= 1 && h[5] >= 1 
+        h[5] -= 1
+        h[10] -= 1
+        h[20] += 1
+      elsif h[5] >= 3
+        h[5] -= 3
+        h[20] += 1
+      else
+        return false
+      end
+    else
+      StandardError
+    end
+  end
+  true
+end
+# @lc code=end
+
