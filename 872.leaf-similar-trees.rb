98543fee37cbc2b0068d475eddb5426371acec18
kotaesaki <jmkamy552@gmail.com>
2023-06-01 08:45:47 +0900

    v3

--- 872.leaf-similar-trees.rb
+++ 872.leaf-similar-trees.rb
@@ -18,45 +18,17 @@
 # @param {TreeNode} root2
 # @return {Boolean}
 def leaf_similar(root1, root2)
-  result1 = []
-  result2 = []
-
-  if root1.left == nil && root1.right == nil
-    result1 << root1.val
-  end
-  if root1.left != nil
-    similar(root1.left, result1)
-  end
-
-  if root1.right != nil
-    similar(root1.right, result1)
-  end
-
-  if root2.left == nil && root2.right == nil
-    result2 << root2.val
-  end
-  if root2.left != nil
-    similar(root2.left, result2)
-  end
-
-  if root2.right != nil
-    similar(root2.right, result2)
-  end
-  puts result1.inspect
-  puts result2.inspect
-  result1 == result2
+  similar(root1) == similar(root2)
 end
 
-def similar(root, result)
+def similar(root)
   if root.left == nil && root.right == nil
-    result << root.val
-    return
-  end
-  if root.left != nil
-    similar(root.left, result)
-  end
-  if root.right != nil
-    similar(root.right, result)
+    [root.val]
+  else
+    result = []
+    result += similar(root.left) if root.left
+    result += similar(root.right) if root.right
+    result
   end
 end
 # @lc code=end
