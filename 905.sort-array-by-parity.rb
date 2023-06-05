219b02cf02b351c1f7cef5b0bf87acbe8e1ed0f0
kotaesaki <jmkamy552@gmail.com>
2023-06-05 22:36:28 +0900

    done

--- 905.sort-array-by-parity.rb
+++ 905.sort-array-by-parity.rb
@@ -8,7 +8,16 @@
 # @param {Integer[]} nums
 # @return {Integer[]}
 def sort_array_by_parity(nums)
-    
+  even = []
+  odd = []
+  nums.each do |n|
+    if n % 2 == 0
+      even << n
+    else
+      odd << n
+    end
+  end
+  even + odd
 end
 # @lc code=end
 
