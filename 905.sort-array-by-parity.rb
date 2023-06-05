9412c06b6ed5ea64f9b6e7deead36903ee8746c7
kotaesaki <jmkamy552@gmail.com>
2023-06-05 21:16:57 +0900

    wip

---
+++ 905.sort-array-by-parity.rb
@@ -0,0 +1,14 @@
+#
+# @lc app=leetcode id=905 lang=ruby
+#
+# [905] Sort Array By Parity
+#
+
+# @lc code=start
+# @param {Integer[]} nums
+# @return {Integer[]}
+def sort_array_by_parity(nums)
+    
+end
+# @lc code=end
+
