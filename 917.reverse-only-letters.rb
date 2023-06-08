1bad26a72adcc768ecdfe8f0051b56a03b6fb383
kotaesaki <jmkamy552@gmail.com>
2023-06-08 17:17:49 +0900

    done in ruby

--- 917.reverse-only-letters.rb
+++ 917.reverse-only-letters.rb
@@ -11,18 +11,20 @@ def reverse_only_letters(s)
   lowers = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r","s", "t", "u","v", "w", "x", "y", "z"]
   uppers = lowers.map(&:upcase)
   arr = lowers + uppers
-  result = {}
-  s.chars.each_with_index {|s, i| result[i] = s}
-  key = 200
-  result.each do |k, v|
-    if arr.include?(v) 
-      result[key] = v
-      result.delete(k)
-      key -= 1
+  result = s.chars
+  left, right = 0, result.length - 1
+  while left < right
+    while left < right && !arr.include?(result[left])
+      left += 1
     end
+    while left < right && !arr.include?(result[right])
+      right -= 1
+    end
+    result[left], result[right] = result[right], result[left]
+    left += 1
+    right -= 1
   end
-  puts result.inspect
-  # result.join!
+  result.join
 end
 # @lc code=end
 
