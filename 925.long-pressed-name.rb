00a958d7b0c658116fb57413f2cba2bdfb70c874
kotaesaki <jmkamy552@gmail.com>
2023-06-10 16:27:07 +0900

    done

--- 925.long-pressed-name.rb
+++ 925.long-pressed-name.rb
@@ -9,7 +9,20 @@
 # @param {String} typed
 # @return {Boolean}
 def is_long_pressed_name(name, typed)
-    
+  return false if name.length > typed.length
+
+  index = 0
+  prev, curernt = nil, name[index]
+  typed.chars.each do |c|
+    if curernt != c
+      return false if prev == nil || prev != c 
+      next
+    end
+    prev = name[index]
+    index += 1
+    curernt = name[index]
+  end
+  name[index..].empty?
 end
 # @lc code=end
 
