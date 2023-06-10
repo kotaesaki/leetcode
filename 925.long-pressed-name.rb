17fb3a543447185214c440d4c6b15189f1d25ab8
kotaesaki <jmkamy552@gmail.com>
2023-06-10 15:20:17 +0900

    wip

---
+++ 925.long-pressed-name.rb
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode id=925 lang=ruby
+#
+# [925] Long Pressed Name
+#
+
+# @lc code=start
+# @param {String} name
+# @param {String} typed
+# @return {Boolean}
+def is_long_pressed_name(name, typed)
+    
+end
+# @lc code=end
+
