dbe71303989d29dfdeaa3500c37cc6ee36db1f54
kotaesaki <jmkamy552@gmail.com>
2023-06-12 08:59:21 +0900

    done in ruby

--- 929.unique-email-addresses.rb
+++ 929.unique-email-addresses.rb
@@ -8,7 +8,40 @@
 # @param {String[]} emails
 # @return {Integer}
 def num_unique_emails(emails)
-    
+  result = []
+  emails.each do |email|
+    a = convert(email)
+    next if result.include?(a)
+    result << a
+  end
+
+  result.size
+end
+
+def convert(email)
+  plus = false
+  arr = []
+  email.chars.each do |char|
+    if plus
+      if char == '@'
+        plus = false
+        arr << char
+      end
+      next
+    end
+    if char.match(/[a-z]/) != nil
+      arr << char
+    elsif arr.include?('@') && char == '.'
+      arr << char
+    elsif arr.include?('@') && char == '+'
+      arr << char
+    elsif char == '+'
+      plus = true
+    elsif char == '@'
+      arr << char
+    end
+  end
+  arr.join
 end
 # @lc code=end
 
