6254caca20153cbe4b65ff2f6212275a3b107c6e
kotaesaki <jmkamy552@gmail.com>
2023-06-14 08:30:23 +0900

    done second

--- 242.valid-anagram.go
+++ 242.valid-anagram.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 /*
  * @lc app=leetcode id=242 lang=golang
  *
@@ -10,36 +8,21 @@ import "fmt"
 
 // @lc code=start
 func isAnagram(s string, t string) bool {
-	// if len(s) != len(t) {
-	// 	return false
-	// }
-	// for _, v := range s {
-	// 	if strings.Contains(t, string(v)) {
-	// 		t = strings.Replace(t, string(v), "", 1)
-	// 	} else {
-	// 		return false
-	// 	}
-	// }
-	// return true
-
-	chars := make([]int, 26)
-	for _, v := range s {
-		i := int(v - 'a')
-		chars[i]++
-		fmt.Println(chars)
+	if len(s) != len(t) {
+		return false
 	}
 
-	for _, v := range t {
-		i := int(v - 'a')
-		chars[i]--
+	a := make(map[rune]int)
+	for _, c := range s {
+		a[c]++
 	}
 
-	for _, v := range chars {
-		if v != 0 {
+	for _, c := range t {
+		if a[c] < 1 {
 			return false
 		}
+		a[c]--
 	}
-
 	return true
 }
 
