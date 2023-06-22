67a0f5ae2d3c80c93c07d67195e370cf04e978d9
kotaesaki <jmkamy552@gmail.com>
2023-06-22 09:17:02 +0900

    done

--- 167.two-sum-ii-input-array-is-sorted.py
+++ 167.two-sum-ii-input-array-is-sorted.py
@@ -7,6 +7,11 @@
 # @lc code=start
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        
+        memo = defaultdict(int)
+        for i, n in enumerate(numbers):
+          diff = target - n
+          if memo[diff] != 0:
+            return [memo[diff], i + 1]
+          memo[n] = i + 1
 # @lc code=end
 
